d2811a639d4c5795018434bddf094038c8e78938
Aditya Narayn <aditya.narayn30@gmail.com>
2020-12-28 23:48:58 +0530

    signin/log out
    Firebase Auth SignIn logout

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,22 +1,50 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { firestoreConnect } from 'react-redux-firebase'
+import { compose } from 'redux'
 
 const ProjectDetails = (props) => {
-    const id = props.match.params.id;
-    return (
+    const {project} = props;
+    //const id = props.match.params.id;
+    //console.log(props);
+    if(project) {
+        return(
         <div className='container section project-details'>
           <div className="card z-depth-0">
               <div className="card-content">
-                  <span className="card-title">Project Title- {id} </span>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+                  <span className="card-title">{project.title} </span>
+                  <p>{ project.content} </p>
               </div>
               <div className="card-action grey lighten-4 grey-text">
-                  <div>Posted By gaara</div>
+                  <div>Posted By- {project.authorFirstName} {project.authorLastName}</div>
                   <div>18th Dec, 2020</div>
               </div>
           </div>
-            
+        </div>
+        )
+    }else{
+    return (
+        <div className="container-center">
+            <p>Loading Content....</p>
         </div>
     )
 }
+}
+const mapStateToProps = (state, ownProps) => {
+    //console.log(state)
+    const id = ownProps.match.params.id;
+    const projects = state.firestore.data.projects;
+    const project = projects ? projects[id] : null
+    return{
+        project: project
 
-export default ProjectDetails
+    }
+}
+
+
+export default compose(
+    connect(mapStateToProps),
+    firestoreConnect([
+        { collection: 'projects' }
+    ])
+)(ProjectDetails)
--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -2,11 +2,14 @@ import authReducer from "./authReducer";
 import projectReducer from './projectReducer';
 import { combineReducers } from 'redux';
 import {firestoreReducer} from 'redux-firestore'
+import { firebaseReducer } from 'react-redux-firebase'
 
 const rootReducer = combineReducers({
     auth: authReducer,
     project: projectReducer,
-    firestore: firestoreReducer //changes
+    firestore: firestoreReducer, //sync redux state with firebase
+    firebase: firebaseReducer
+
 });
 
 export default rootReducer;
